dbab1423202970a3816157615e358c4b3e8643b9
Chris <chrismdowning@gmail.com>
2023-08-07 21:47:12 -0700

    Initial commit.  Built the main window of the GUI which will display the database table

---
+++ README.md
@@ -0,0 +1,5 @@
+<h2>Instructor-Guided App 13</h2>
+<h1>Student Management System</h1>
+<body>A desktop database program used to management student enrollments.</body>
+<h3>Learning objective:</h3>
+<p>- Utilize PyQt6 library, classes, and most other previously-learned concepts.</p>
---
+++ main.py
@@ -0,0 +1,36 @@
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
+    QLineEdit, QPushButton, QMainWindow, QTableWidget
+from PyQt6.QtGui import QAction
+import sys
+import sqlite3
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+        main_grid = QGridLayout()
+
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile"))
+        self.setCentralWidget(self.table)
+
+
+    def load_data(self):
+        self.table
+
+
+app = QApplication(sys.argv)
+student_mgmt_sys = MainWindow()
+student_mgmt_sys.show()
+sys.exit(app.exec())
